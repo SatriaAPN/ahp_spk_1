075b4efc49eaae28e2b1edcbb0ada134456f81fb
SatriaAPN <satrianusa10@gmail.com>
2023-02-11 19:53:37 +0700

    add cors

--- index.js
+++ index.js
@@ -155,6 +155,12 @@ const {
   periksaApakahSemuaKriteriaTidakMemilikiError
 } = require('./functions');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
